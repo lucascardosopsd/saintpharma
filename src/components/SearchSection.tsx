8fb0d9d34a580e79a0f91b2a62f7add4949a8a8b
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-11-14 00:07:02 -0300

    update: search section layout

--- src/components/SearchSection.tsx
+++ src/components/SearchSection.tsx
@@ -22,22 +22,27 @@ const SearchSection = () => {
 
   return (
     <div className="flex flex-col  justify-center bg-primary p-5 h-[300px] gap-10">
-      <p className="text-5xl text-background font-semibold">
-        O que você deseja aprender hoje?
-      </p>
-      <div className="flex relative items-center">
-        <Input
-          className="border-background border-0 rounded-none border-b placeholder:text-background font-semibold placeholder:font-normal"
-          placeholder="Busque por um curso"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          onKeyDown={(e) => {
-            e.key == "Enter" && handleSearch();
-          }}
-        />
+      <div className="w-full max-w-[500px] mx-auto">
+        <p className="text-5xl text-background font-semibold">
+          O que você deseja aprender hoje?
+        </p>
+        <div className="flex relative items-center">
+          <Input
+            className="border-background border-0 rounded-none border-b placeholder:text-background font-semibold placeholder:font-normal"
+            placeholder="Busque por um curso"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            onKeyDown={(e) => {
+              e.key == "Enter" && handleSearch();
+            }}
+          />
 
-        <div className="text-background cursor-pointer" onClick={handleSearch}>
-          <Search />
+          <div
+            className="text-background cursor-pointer"
+            onClick={handleSearch}
+          >
+            <Search />
+          </div>
         </div>
       </div>
     </div>
