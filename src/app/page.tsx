dce5c669cfa80dac0379084ce27c505ac1d75ff2
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-11-14 00:07:14 -0300

    update: course card layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,15 @@ export default async function Home() {
       <Header user={user} />
       <div className="h-[92svh] overflow-y-auto">
         <SearchSection />
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
+        <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 tablet:gap-2 tablet:p-2">
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+        </div>
       </div>
     </div>
   );
--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 
 const CourseCard = () => {
   return (
-    <div className="h-[250px] w-full relative flex items-end">
+    <div className="h-[250px] w-full relative flex items-end tablet:rounded group overflow-hidden cursor-pointer">
       <Image
         src="https://i.imgur.com/SVwi47c.jpeg"
         alt="Imagem curso"
         height={1000}
         width={1000}
-        className="w-full h-full object-cover absolute -z-50 left-0 top-0"
+        className="w-full h-full object-cover absolute -z-50 left-0 top-0 tablet:rounded group-hover:scale-125 transition-all"
       />
 
-      <div className="h-full w-full absolute left-0 top-0 bg-gradient-to-t from-primary to-transparent z-10" />
+      <div className="h-full w-full absolute left-0 top-0 bg-gradient-to-t from-primary to-transparent z-10 tablet:rounded" />
 
       <div className="flex justify-between text-background p-5 w-full z-50 ">
         <p className="text-semibold">Nome do curso</p>
