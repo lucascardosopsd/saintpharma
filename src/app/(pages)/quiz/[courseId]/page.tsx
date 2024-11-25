e9c8268d784dd7f7df77d058182fbb90e24c6149
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-11-25 15:33:43 -0300

    update: quiz page layout

--- src/app/(pages)/quiz/[courseId]/page.tsx
+++ src/app/(pages)/quiz/[courseId]/page.tsx
@@ -2,7 +2,6 @@ import { getCourseById } from "@/actions/courses/getId";
 import { getQuizByCourseId } from "@/actions/quiz/getByCourseId";
 import { getUserByClerk } from "@/actions/user/getUserByClerk";
 import Exam from "@/components/Exam";
-import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 
 type QuizPageProps = {
@@ -22,12 +21,7 @@ const QuizPage = async ({ params }: QuizPageProps) => {
 
   const quiz = await getQuizByCourseId({ id: courseId });
 
-  return (
-    <div>
-      <Header backIcon />
-      <Exam quiz={quiz} course={course} userId={user?.id!} />
-    </div>
-  );
+  return <Exam quiz={quiz} course={course} userId={user?.id!} />;
 };
 
 export default QuizPage;
