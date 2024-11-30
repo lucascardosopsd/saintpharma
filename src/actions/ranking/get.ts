99e5828ae3e8e3956bc5224feca67485c604ce59
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-11-30 14:55:06 -0300

    refact: get ranking

---
+++ src/actions/ranking/get.ts
@@ -0,0 +1,64 @@
+"use server";
+
+import { Certificate, User } from "@prisma/client";
+import { endOfWeek, startOfWeek } from "date-fns";
+import { getManyCertificates } from "../certification/getManyCertificates";
+import { ptBR } from "date-fns/locale";
+
+type CustomCertificateProps = Certificate & {
+  User: User;
+};
+
+type CustomCertificatesReturn = {
+  certificates: CustomCertificateProps[];
+  pages: number;
+};
+
+type UserPoints = {
+  [key: string]: {
+    userId: string;
+    name: string;
+    points: number;
+    profileImage: string;
+  };
+};
+
+export const getRanking = async () => {
+  const today = new Date();
+
+  const firstDayOfWeek = startOfWeek(today, { locale: ptBR, weekStartsOn: 0 });
+
+  const lastDayOfWeek = endOfWeek(today, { locale: ptBR, weekStartsOn: 0 });
+
+  const { certificates } = await getManyCertificates<CustomCertificatesReturn>({
+    page: 0,
+    take: 50,
+    query: {
+      where: {
+        createdAt: {
+          lte: new Date(lastDayOfWeek),
+          gte: new Date(firstDayOfWeek),
+        },
+      },
+      include: {
+        User: true,
+      },
+    },
+  });
+
+  const userPoints = certificates.reduce((acc: UserPoints, certificate) => {
+    const user = certificate.User;
+    if (!user) return acc;
+
+    acc[user.id] = {
+      userId: user.id,
+      name: user.name! || "",
+      points: user.points,
+      profileImage: user.profileImage!,
+    };
+
+    return acc;
+  }, {});
+
+  return Object.values(userPoints).sort((a, b) => b.points - a.points);
+};
--- src/app/(pages)/ranking/page.tsx
+++ src/app/(pages)/ranking/page.tsx
@@ -1,69 +1,10 @@
-import { getManyCertificates } from "@/actions/certification/getManyCertificates";
+import { getRanking } from "@/actions/ranking/get";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Card, CardHeader } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
-import { Certificate, User } from "@prisma/client";
-import { startOfWeek, endOfWeek } from "date-fns";
-import { ptBR } from "date-fns/locale";
-
-type CustomCertificateProps = Certificate & {
-  User: User;
-};
-
-type CustomCertificatesReturn = {
-  certificates: CustomCertificateProps[];
-  pages: number;
-};
-
-type UserPoints = {
-  [key: string]: {
-    userId: string;
-    name: string;
-    points: number;
-    profileImage: string;
-  };
-};
 
 const ProfilePage = async () => {
-  const today = new Date();
-
-  const firstDayOfWeek = startOfWeek(today, { locale: ptBR, weekStartsOn: 0 });
-
-  const lastDayOfWeek = endOfWeek(today, { locale: ptBR, weekStartsOn: 0 });
-
-  const { certificates } = await getManyCertificates<CustomCertificatesReturn>({
-    page: 0,
-    take: 50,
-    query: {
-      where: {
-        createdAt: {
-          lte: new Date(lastDayOfWeek),
-          gte: new Date(firstDayOfWeek),
-        },
-      },
-      include: {
-        User: true,
-      },
-    },
-  });
-
-  const userPoints = certificates.reduce((acc: UserPoints, certificate) => {
-    const user = certificate.User;
-    if (!user) return acc;
-
-    acc[user.id] = {
-      userId: user.id,
-      name: user.name! || "",
-      points: user.points,
-      profileImage: user.profileImage!,
-    };
-
-    return acc;
-  }, {});
-
-  const sortedUsers = Object.values(userPoints).sort(
-    (a, b) => b.points - a.points
-  );
+  const ranking = await getRanking();
 
   return (
     <div className="h-[92svh] overflow-y-auto w-full flex flex-col items-center p-5">
@@ -71,7 +12,7 @@ const ProfilePage = async () => {
       <p className="text-lg text-primary">Os 50 melhores</p>
       <p className="text-muted-foreground">Últimos 7 Dias</p>
 
-      {sortedUsers.map((user, index) => (
+      {ranking.map((user, index) => (
         <Card className="w-full mt-5" key={index}>
           <CardHeader className="flex-row justify-between w-full">
             <div className="flex items-center gap-2">
