1f31ebca633950f5ad86e8f9d144ee442147f70f
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-12-03 20:02:14 -0300

    fix: get user by clerk action

--- src/actions/user/getUserByClerk.ts
+++ src/actions/user/getUserByClerk.ts
@@ -5,5 +5,10 @@ import { currentUser } from "@clerk/nextjs/server";
 
 export const getUserByClerk = async () => {
   const clerk = await currentUser();
+
+  if (!clerk) {
+    return null;
+  }
+
   return await prisma.user.findUnique({ where: { clerkId: clerk?.id } });
 };
