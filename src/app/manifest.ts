3fb73c25bb9c86449fd09b8028937e719e132049
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-12-03 18:22:56 -0300

    feat: chrome PWA

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "SaintPharma PWA",
+    short_name: "SaintPharma",
+    description: "Aprenda com quem sabe",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#0ea5e9",
+    theme_color: "#ffff",
+    icons: [
+      {
+        src: "/logo-192.ico",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-256.ico",
+        sizes: "256x256",
+        type: "image/png",
+      },
+    ],
+  };
+}
