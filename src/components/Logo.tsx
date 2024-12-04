3ad9528309795b8e9259eed9edbd3817100b08f7
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-12-03 21:16:54 -0300

    update: logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Logo = () => {
   return (
-    <Link href="/">
+    <Link href="/" className="flex gap-1 items-center">
+      <Image
+        src="/logo.png"
+        alt="Logo"
+        height={1000}
+        width={1000}
+        className="h-10 w-auto"
+      />
       <p className="font-bold text-primary">SaintPharma</p>
     </Link>
   );
