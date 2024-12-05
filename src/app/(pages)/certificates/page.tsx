0f1f1a45337741b8fcd6fb8fad11860efb416c86
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-12-04 22:16:14 -0300

    update: certificates page layout

--- src/app/(pages)/certificates/page.tsx
+++ src/app/(pages)/certificates/page.tsx
@@ -13,24 +13,28 @@ const Page = async () => {
   });
 
   return (
-    <div className="flex flex-col gap-2 h-[92svh] overflow-y-auto p-5">
-      <p className="text-2xl font-semibold text-primary">Certificados</p>
-      {certificates.map((certificate) => (
-        <Link href={`/certificate/${certificate.id}`} key={certificate.id}>
-          <Card className="cursor-pointer group hover:bg-primary transition">
-            <CardHeader>
-              <CardTitle className="flex justify-between font-normal items-center group-hover:text-background">
-                <div className="flex gap-2 items-center">
-                  <p className="font-semibold">{certificate.courseTitle}</p>
-                  <p>|</p>
-                  <p>{certificate.workload}(Hrs)</p>
-                </div>
-                <p>{format(new Date(certificate!.createdAt), "dd/MM/yyyy")}</p>
-              </CardTitle>
-            </CardHeader>
-          </Card>
-        </Link>
-      ))}
+    <div className="max-w-lg mx-auto w-full">
+      <div className="flex flex-col gap-2 h-[92svh] overflow-y-auto p-5">
+        <p className="text-2xl font-semibold text-primary">Certificados</p>
+        {certificates.map((certificate) => (
+          <Link href={`/certificate/${certificate.id}`} key={certificate.id}>
+            <Card className="cursor-pointer group hover:bg-primary transition">
+              <CardHeader>
+                <CardTitle className="flex justify-between font-normal items-center group-hover:text-background">
+                  <div className="flex gap-2 items-center">
+                    <p className="font-semibold">{certificate.courseTitle}</p>
+                    <p>|</p>
+                    <p>{certificate.workload}(Hrs)</p>
+                  </div>
+                  <p>
+                    {format(new Date(certificate!.createdAt), "dd/MM/yyyy")}
+                  </p>
+                </CardTitle>
+              </CardHeader>
+            </Card>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
