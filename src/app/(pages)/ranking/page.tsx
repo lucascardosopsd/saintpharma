e670f1fb78698c07da3da2b21ee030034bff1a5d
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-12-04 22:12:10 -0300

    update: ranking layout

--- src/app/(pages)/ranking/page.tsx
+++ src/app/(pages)/ranking/page.tsx
@@ -12,33 +12,35 @@ const ProfilePage = async () => {
       <p className="text-lg text-primary">Os 50 melhores</p>
       <p className="text-muted-foreground">Últimos 7 Dias</p>
 
-      {ranking.map((user, index) => (
-        <Card className="w-full mt-5" key={index}>
-          <CardHeader className="flex-row justify-between w-full">
-            <div className="flex items-center gap-2">
-              <p className="text-muted-foreground">#{index + 1}</p>
+      <div className="mx-auto max-w-lg w-full">
+        {ranking.map((user, index) => (
+          <Card className="w-full mt-5" key={index}>
+            <CardHeader className="flex-row justify-between w-full">
+              <div className="flex items-center gap-2">
+                <p className="text-muted-foreground">#{index + 1}</p>
 
-              <Separator orientation="vertical" />
+                <Separator orientation="vertical" />
 
-              {user.profileImage ? (
-                <Avatar className="h-16 w-16">
-                  <AvatarImage src={user.profileImage} />
-                </Avatar>
-              ) : (
-                <div className="rounded-full h-16 w-16 relative flex items-center justify-center border border-primary">
-                  <p className="text-4xl text-primary font-bold">
-                    {user.name[0]}
-                  </p>
-                </div>
-              )}
+                {user.profileImage ? (
+                  <Avatar className="h-16 w-16">
+                    <AvatarImage src={user.profileImage} />
+                  </Avatar>
+                ) : (
+                  <div className="rounded-full h-16 w-16 relative flex items-center justify-center border border-primary">
+                    <p className="text-4xl text-primary font-bold">
+                      {user.name[0]}
+                    </p>
+                  </div>
+                )}
 
-              <p className="text-primary font-bold">{user.name}</p>
-            </div>
+                <p className="text-primary font-bold">{user.name}</p>
+              </div>
 
-            <p className="font-bold flex items-center">{user.points} Pts</p>
-          </CardHeader>
-        </Card>
-      ))}
+              <p className="font-bold flex items-center">{user.points} Pts</p>
+            </CardHeader>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
