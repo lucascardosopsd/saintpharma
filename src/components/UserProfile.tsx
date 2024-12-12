01b6670fad9a9284762c9d5a6892ab3c9d134e04
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-12-12 14:50:38 -0300

    feat: update user image profile

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -12,6 +12,7 @@ import { z } from "zod";
 import { User as ClerkUser } from "@clerk/nextjs/server";
 import { revalidateRoute } from "@/actions/revalidateRoute";
 import { toast } from "sonner";
+import { ChangeEvent, useState } from "react";
 
 type UserProfileProps = {
   user: User;
@@ -21,6 +22,7 @@ type UserProfileProps = {
 
 const UserProfile = ({ user, serverClerkUser, points }: UserProfileProps) => {
   const { user: clerkUser } = useUser();
+  const [newImageFile, setNewImageFile] = useState<File | null>(null);
 
   const form = useUserProfileForm({
     defaultValues: {
@@ -43,20 +45,57 @@ const UserProfile = ({ user, serverClerkUser, points }: UserProfileProps) => {
     }
   };
 
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setNewImageFile(e.target.files[0]);
+    }
+  };
+
+  const updateUserImage = async () => {
+    try {
+      await clerkUser?.setProfileImage({
+        file: newImageFile,
+      });
+
+      revalidateRoute({ fullPath: "/profile" });
+
+      toast.success("Imagem atualizada");
+    } catch (error) {
+      toast.error("Erro ao atualizar imagem");
+    }
+  };
+
   return (
     <Form {...form}>
       <form
         className="flex flex-col items-center justify-center gap-2 max-w-lg w-full px-5"
         onSubmit={form.handleSubmit(onSubmit)}
       >
-        <Avatar className="h-44 w-44">
-          <AvatarImage
-            src={clerkUser?.imageUrl}
-            alt="Imagem do usuário"
-            height={1000}
-            width={1000}
-          />
-        </Avatar>
+        <div className="flex items-center justify-center flex-col gap-2">
+          <Avatar className="h-44 w-44">
+            <AvatarImage
+              src={clerkUser?.imageUrl}
+              alt="Imagem do usuário"
+              height={1000}
+              width={1000}
+            />
+          </Avatar>
+
+          {!newImageFile && (
+            <div className="relative w-full">
+              <div className="w-full h-10 rounded border border-border  flex items-center justify-center cursor-pointer">
+                <p className="text-sm text-primary/80">Atualizar imagem</p>
+              </div>
+              <Input
+                type="file"
+                className="absolute w-full top-0 bottom-0 m-0 bg-red-500 opacity-0 cursor-pointer"
+                onChange={handleFileChange}
+              />
+            </div>
+          )}
+
+          {newImageFile && <Button onClick={updateUserImage}>Confirmar</Button>}
+        </div>
         <div>
           <div className="text-xl flex gap-2 font-semibold text-center text-primary">
             <p>{clerkUser?.firstName}</p>
