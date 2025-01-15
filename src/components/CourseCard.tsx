d1103f2bdf41f33bee7b3ac137dd1040373db303
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-01-15 16:00:53 -0300

    update: course card layout

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -78,7 +78,7 @@ const CourseCard = ({ course, userPoints }: CourseCardProps) => {
 
           <div
             className={cn(
-              "h-full w-full absolute left-0 top-0 bg-gradient-to-t from-primary via-transparent to-transparent z-10 tablet:rounded"
+              "h-full w-full absolute left-0 top-0 bg-gradient-to-t from-blue-800 via-transparent to-transparent z-10 tablet:rounded"
             )}
           />
 
